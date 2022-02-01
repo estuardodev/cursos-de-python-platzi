0c4ecfecf5672ac6f49f4e13f22634a2da6f4c5e
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-01-31 19:49:59 -0600

    Tercer commit del curso de python

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Curso Básico de Python
 En este repositorio estaran subidos cada proyecto que se realizara en el Curso de Programación Básica de Python de Platzi
+
+##En este curso de Programación Básica en Python estoy aprendiendo lo siguiente:
+- Impresiones en pantalla
+- Entradas de usuario
+- Comentarios
+- Condicionales
--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-quetzales = input("¿Cuantos quetzales tienes?: ")
-quetzales = float(quetzales)
-valor_dolar = 7.77
-dolares = quetzales / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes  $" + dolares + " dolares" )
+menu = """
+Bienvenido al conversor de monedas <3
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Quetzales
+
+Elige una opción: 
+"""
+opcion = int(input(menu))
+
+if opcion == 1:
+    quetzales = input("¿Cuantos Pesos Colombianos tienes?: ")
+    quetzales = float(quetzales)
+    valor_dolar = 3943.02
+    dolares = quetzales / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  $" + dolares + " dolares" )
+elif opcion == 2:
+    quetzales = input("¿Cuantos Pesos Argentinos tienes?: ")
+    quetzales = float(quetzales)
+    valor_dolar = 105.19
+    dolares = quetzales / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  $" + dolares + " dolares" )
+elif opcion == 3:
+    quetzales = input("¿Cuantos quetzales tienes?: ")
+    quetzales = float(quetzales)
+    valor_dolar = 7.77
+    dolares = quetzales / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  $" + dolares + " dolares" )
+else:
+    print("Ingresa una opción correcta.")
