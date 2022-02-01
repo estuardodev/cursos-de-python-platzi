f6ba7e5a1ff47ddf355a318cc75f261110d0a939
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-01-31 18:49:26 -0600

    Primer Commit del proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Conversor de QTM a USD
+Este conversor es el proyecto realizado en el Curso de Programación en Python.
---
+++ conversor.py
@@ -0,0 +1,7 @@
+quetzales = input("¿Cuantos quetzales tienes?: ")
+quetzales = float(quetzales)
+valor_dolar = 7.77
+dolares = quetzales / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes  $" + dolares + " dolares" )
