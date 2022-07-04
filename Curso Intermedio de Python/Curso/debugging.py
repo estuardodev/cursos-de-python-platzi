0aa11bde81f537d13abacdbe9866f5f7d43a5649
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-07-04 16:30:32 -0600

    Ejercicio manejos de errores

--- Curso Intermedio de Python/Curso/debugging.py
+++ Curso Intermedio de Python/Curso/debugging.py
@@ -1,15 +1,25 @@
 def divisor(num):
     divisors =[]
 
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError("Ingresaste un numero negativo")
+        else:
+            for i in range(1, num + 1):
+                if num % i == 0:
+                    divisors.append(i)
+            return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
 
 def run():
-    num = int(input("Ingresa un numero: "))
-    print(divisor(num))
-    print("Termino")
+    try:
+        num = int(input("Ingresa un numero: "))
+        print(divisor(num))
+        print("Termino")
+    except ValueError:
+        print("Error, ingresaste un caracter incorrecto")
 
 if __name__ == "__main__":
     run()
